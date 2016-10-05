06716d55ad242441cc825c01b78ee11ced973719
Donal Maher <t00182654@365s.ittralee.ie>
2016-10-05 12:59:37 +0100

    Initial commit

---
+++ test/GuitarTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by D on 05/10/2016.
+ */
+public class GuitarTest {
+    @Test
+    public void getPrice() throws Exception {
+
+    }
+
+}
